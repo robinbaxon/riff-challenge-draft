44eef9198f3841d8d32811796666c45f8f8eac49
Robin Hansen <Robin.Hansen@oxx.no>
2016-10-26 09:29:43 +0200

    Add comments

--- RiffChallengeDraft/Program.cs
+++ RiffChallengeDraft/Program.cs
@@ -4,6 +4,8 @@ namespace RiffChallengeDraft
 {
     class Program
     {
+    	//Comment about things and other things
+	// No, lol
         static void Main(string[] args)
         {
             var draftenabler = new DraftFacilitator();
